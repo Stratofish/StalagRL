f8f6a65a797f39d38b4ad07b887f5ccb89f9a15a
dan <dan@unallocated.barefruit.co.uk>
2014-03-08 23:46:08 +0000

    10x10 floor.

--- src/com/etrium/stalagrl/Map.java
+++ src/com/etrium/stalagrl/Map.java
@@ -1,13 +1,13 @@
 package com.etrium.stalagrl;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.assets.AssetManager;
-import com.badlogic.gdx.assets.loaders.ModelLoader;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL10;
 import com.badlogic.gdx.graphics.PerspectiveCamera;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.VertexAttributes.Usage;
 import com.badlogic.gdx.graphics.g3d.Environment;
 import com.badlogic.gdx.graphics.g3d.Material;
 import com.badlogic.gdx.graphics.g3d.Model;
@@ -17,7 +17,6 @@ import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
 import com.badlogic.gdx.graphics.g3d.attributes.TextureAttribute;
 import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
 import com.badlogic.gdx.graphics.g3d.utils.GLES10ShaderProvider;
-import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
 
 public class Map
 {
@@ -26,9 +25,7 @@ public class Map
 	public String curLevel;
 	
 	private PerspectiveCamera camera = null;
-	private Model model;
-	private ModelInstance instance = null;
-	private ModelInstance instance2 = null;
+	private List<ModelInstance> floorTiles;
 	private ModelBatch modelBatch;
 	private Environment environment;
 	private AssetManager assets;
@@ -55,30 +52,41 @@ public class Map
 		
 		dirtTexture = new Texture(Gdx.files.internal("data/textures/dirt.png"));
 		
-		ModelBuilder modelBuilder = new ModelBuilder();
-        model = modelBuilder.createBox(0.5f, 0.5f, 0.5f, 
-            new Material(ColorAttribute.createDiffuse(Color.GREEN)),
-            Usage.Position | Usage.Normal);
-
-        instance2 = new ModelInstance(model);
+		floorTiles = new ArrayList<ModelInstance>();
 	}
 	
 	public void Render()
 	{
-		if (assets.update()) {
-			instance = new ModelInstance(assets.get("data/models/plain-floor.g3db", Model.class));
-			Material mat = instance.materials.get(0);
-			mat.set(TextureAttribute.createDiffuse(dirtTexture));
+		if ((floorTiles.size() == 0) &&
+			(assets.update()))
+		{
+			for (int i = 0; i < 10; i++)
+			{
+				for (int j = 0; j < 10; j++)
+				{
+					ModelInstance instance = new ModelInstance(assets.get("data/models/plain-floor.g3db", Model.class));
+					
+					instance.getNode("Plane").translation.x = 5.0f-i;
+					instance.getNode("Plane").translation.y = 5.0f-j;
+					instance.getNode("Plane").calculateTransforms(false);
+					
+					Material mat = instance.materials.get(0);
+					mat.set(TextureAttribute.createDiffuse(dirtTexture));
+					
+					floorTiles.add(instance);
+				}
+			}
 		}
 		
 		Gdx.gl.glViewport(0, 0, 1024, 768);
 		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
 
-		if (instance != null)
+		if (floorTiles.size() > 0)
 		{
 			modelBatch.begin(camera);
-			modelBatch.render(instance, environment);
-			//modelBatch.render(instance2, environment);
+			for (int i = 0; i < floorTiles.size(); i++)
+				modelBatch.render(floorTiles.get(i), environment);
+			
 			modelBatch.end();
 		}
 	}
