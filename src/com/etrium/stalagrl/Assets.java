5a340cc87e08542c895cf144e4ff8a12b15f1d96
Richard Talbutt <richard@talbutt.co.uk>
2014-03-09 14:09:43 +0000

    Added stone

--- src/com/etrium/stalagrl/Assets.java
+++ src/com/etrium/stalagrl/Assets.java
@@ -10,8 +10,12 @@ public class Assets
   // Texture Constants
   final static String textureDirt  = Assets.texturePath + "/Dirt.png";
   final static String textureGrass = Assets.texturePath + "/Grass.png";
+  final static String textureStones = Assets.texturePath + "/Stones.png";
+  final static String textureConcrete = Assets.texturePath + "/Concrete.png";
  
   // Model Constants
   final static String modelDirt = Assets.modelPath + "/plain-floor.g3db";
   final static String modelGrass = Assets.modelPath + "/plain-floor.g3db";
+  final static String modelStones = Assets.modelPath + "/plain-floor.g3db";
+  final static String modelConcrete = Assets.modelPath + "/plain-floor.g3db";
 }
