2e5b7836712b6281650dc83072b37384ca2e0c9b
Richard Talbutt <richard@talbutt.co.uk>
2014-03-10 15:40:36 +0000

    Collision Detection added to player.   Map changed to have collision settings for edge of map area.

--- src/com/etrium/stalagrl/MapCell.java
+++ src/com/etrium/stalagrl/MapCell.java
@@ -2,6 +2,11 @@ package com.etrium.stalagrl;
 
 public class MapCell
 {
+  static public final int NORTH = 1;
+  static public final int EAST  = 2;
+  static public final int SOUTH = 4;
+  static public final int WEST  = 8;
+  
 	public int type;
 	public float floorLevel;
 	public int collision;
